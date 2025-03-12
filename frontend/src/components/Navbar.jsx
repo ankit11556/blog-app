7e4d2e47ee2706933ee0107d6f4b374af63bc832
ankit <saharan442090@.com>
2025-03-12 11:16:03 +0530

    add navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,18 @@
-import { Container,Group, } from "@mantine/core";
+import { Container,Group,Title } from "@mantine/core";
 const Navbar = () =>{
   return (
-  <div>
-    <Container>
-     <Group>
-      
-     </Group>
+    <div className="bg-blue-600 w-full">
+    <Container className="w-full p-4">
+      <Group className="w-full flex justify-between items-center ">
+        <Title >My Blog</Title>
+        <ul className="text-white p-2 flex space-x-16 ml-6 font-sans text-lg font-bold">
+        <li className="hover:text-gray-300 cursor-pointer hover:underline">Home</li>
+        <li className="hover:text-gray-300 cursor-pointer hover:underline">Categories</li>
+        <li className="hover:text-gray-300 cursor-pointer hover:underline">About</li>
+        <li className="hover:text-gray-300 cursor-pointer hover:underline">Sign Up</li>
+        <li className="hover:text-gray-300 cursor-pointer hover:underline">Login</li>
+        </ul>
+      </Group>
     </Container>
   </div>
   )
