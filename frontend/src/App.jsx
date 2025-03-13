31a0ff471bab368183cf89eebb7e053e0454eef8
ankit <saharan442090@.com>
2025-03-13 20:14:27 +0530

    add pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
- import BlogSection from "./components/BlogSection"
-
+// import BlogSection from "./components/BlogSection"
+import {Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Categories from "./Pages/Categories";
+import About from "./Pages/About";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
 
 function App() {
   
 
   return (
     <>
+     
      <Navbar></Navbar>
-     <Hero></Hero>
-     <BlogSection></BlogSection>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
      
     </>
   )
